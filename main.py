5cc511026df82653fddfb1640c02683a70144294
David Jeannette <31715021+dajea24@users.noreply.github.com>
2022-02-22 18:47:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# pip install streamlit fbprophet yfinance plotly
+
 import streamlit as st
 from datetime import date
 import pandas as pd
@@ -67,6 +67,3 @@ st.write(f'Forecast plot for {n_days} days')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-# st.write("Forecast components")
-# fig2 = m.plot_components(forecast)
-# st.write(fig2)
